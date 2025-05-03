0df9452af13b90abb0b2e8e22d6d55dd0d1103bc
azoong <shshinkitec2@gmail.com>
2025-05-03 15:18:00 +0900

    코드오류 수정정

--- 재귀/재귀.js
+++ 재귀/재귀.js
@@ -8,11 +8,3 @@ function countdown(number){
 
 
 console.log(countdown(5))
-
-
-function 재귀(array){
-    for (let temp of array){
-        if(temp.length>1) 재귀(temp)
-        else console.log(temp)
-     }
-}
--- 재귀/피보나치(재귀없이).js
+++ 재귀/피보나치(재귀없이).js
@@ -14,3 +14,4 @@ function fib(n){
 console.log(fib(3))
 console.log(fib(5))
 console.log(fib(10))
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
